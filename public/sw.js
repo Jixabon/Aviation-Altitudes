b17c7600fcebe7297483f97f8cb097fb30efe85d
Shawn Holmes <sherlock656@gmail.com>
2022-03-02 20:57:43 -0600

    up version for core change

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const OFFLINE_VERSION = '1.5.0';
+const OFFLINE_VERSION = '1.5.1';
 const CACHE_PREFIX = 'avi-alts';
 const CORE_CACHE = CACHE_PREFIX + '-core-' + OFFLINE_VERSION;
 const STATIC_CACHE = CACHE_PREFIX + '-static';
